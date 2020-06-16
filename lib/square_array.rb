46296174abe30c3d8cff6dee937a98fa283dc6cc
Kyle Walters <kyle.p.walters@gmail.com>
2020-06-16 00:57:05 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -6,12 +6,4 @@ def square_array(numbers)
    puts result
   counter += 1
  end
-  counter = 0 
-  result = Array.new(numbers.size)
-    while counter < numbers.size do
-     result[counter] = numbers[counter] ** 2
-      puts result
-      counter += 1 
-      
-end
 end
